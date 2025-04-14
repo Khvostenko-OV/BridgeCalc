b247804e7aad9216b693549b14db8e558ac3e354
Khvostenko-OV <xboctehko@yandex.ru>
2025-04-14 23:43:50 +0200

    Dotenv

--- main.py
+++ main.py
@@ -1,9 +1,38 @@
+import os
+
 import telebot
 from telebot.types import InlineKeyboardButton as button
+from dotenv import load_dotenv
 
-BOT_TOKEN = '7286761979:AAEkD0NEJ8LquRpTpz40VwCbIFDPZcHbL38'
+load_dotenv(verbose=False, override=True)  # take environment variables from .env
 
-bot = telebot.TeleBot(BOT_TOKEN)
+bot = telebot.TeleBot(os.getenv("BOT_TOKEN",""))
+
+POINTS = {'c': 20, 'd': 20, 'h': 30, 's': 30, 'n': 30}
+SUITS = {'c': '♣️', 'd': '♦️', 'h': '♥️', 's': '♠️', 'n': 'БК'}
+BONUS = {1: '', 2: 'на контре', 4: 'на реконтре'}
+VULNERABLE = {False: 'до зоны', True: 'в зоне'}
+GAME = [300, 500]
+SLAM = [500, 750]
+GSLAM = [1000, 1500]
+OVER_TRICK = [50, 100]
+UNDER_TRICK = [
+    [0, -50, -150, -250, -400, -550, -700, -850, -1000],
+    [0, -100, -250, -400, -550, -700, -850, -1000, -1150],
+]
+
+COMPENSA = [
+    [1200,1200,1200,1200,1200,1100,1000,900,750,600,490,460,430,400,350,300,200,110,70,50,0,
+     -50,-70,-110,-200,-300,-350,-400,-430,-460,-490,-600,-750,-900,-1000,-1100,-1200,-1200,-1200,-1200,-1200
+    ],
+    [1800,1800,1800,1800,1800,1650,1500,1350,1050,900,690,660,630,600,520,440,200,110,70,50,0,
+    -50,-70,-110,-200,-440,-520,-600,-630,-660,-690,-900,-1050,-1350,-1500,-1650,-1800,-1800,-1800,-1800,-1800
+    ],
+]
+
+IMP_TABLE = [0, 20, 50, 90, 130, 170, 220, 270, 320, 370, 430, 500, 600, 750, 900,
+             1100, 1300, 1500, 1750, 2000, 2250, 2500, 3000, 3500, 4000
+            ]
 
 level = 1
 suit = 'c'
@@ -12,33 +41,17 @@ tricks = 0
 vul = False
 counter = 70
 
-points = {'c': 20, 'd': 20, 'h': 30, 's': 30, 'n': 30}
-suits = {'c': '♣️', 'd': '♦️', 'h': '♥️', 's': '♠️', 'n': 'БК'}
-bonus = {1: '', 2: 'на контре', 4: 'на реконтре'}
-vulnerable = {False: 'до зоны', True: 'в зоне'}
-game = [300, 500]
-slam = [500, 750]
-gslam = [1000, 1500]
-over_trick = [50, 100]
-under_trick = [[0,-50,-150,-250,-400,-550,-700,-850,-1000], [0,-100,-250,-400,-550,-700,-850,-1000,-1150]]
-compensa = [
-  [1200,1200,1200,1200,1200,1100,1000,900,750,600,490,460,430,400,350,300,200,110,70,50,0,-50,-70,-110,-200,-300,-350,-400,-430,-460,-490,-600,-750,-900,-1000,-1100,-1200,-1200,-1200,-1200,-1200],
-  [1800,1800,1800,1800,1800,1650,1500,1350,1050,900,690,660,630,600,520,440,200,110,70,50,0,-50,-70,-110,-200,-440,-520,-600,-630,-660,-690,-900,-1050,-1350,-1500,-1650,-1800,-1800,-1800,-1800,-1800]
-]
-imp_table = [0,20,50,90,130,170,220,270,320,370,430,500,600,750,900,1100,1300,1500,1750,2000,2250,2500,3000,3500,4000]
-
-
 def make(level, suit, factor, vul, tricks) -> int:
-    sum = points[suit] * level
+    sum = POINTS[suit] * level
     if suit == 'n':
         sum += 10
     sum *= factor
     if sum >= 100:
-        sum += game[vul]
+        sum += GAME[vul]
     if level == 6:
-        sum += slam[vul]
+        sum += SLAM[vul]
     if level == 7:
-        sum += gslam[vul]
+        sum += GSLAM[vul]
     if sum < 100:
         sum += 50
     if factor == 2:
@@ -46,23 +59,23 @@ def make(level, suit, factor, vul, tricks) -> int:
     if factor == 4:
         sum += 100
     if factor == 1:
-        sum += tricks * points[suit]
+        sum += tricks * POINTS[suit]
     else:
-        sum += tricks * over_trick[vul] * factor
+        sum += tricks * OVER_TRICK[vul] * factor
     return sum
 
 
 def down(factor, vul, tricks: int) -> int:
     if factor == 1:
-        return tricks * over_trick[vul]
+        return tricks * OVER_TRICK[vul]
     else:
-        return under_trick[vul][-tricks] * factor
+        return UNDER_TRICK[vul][-tricks] * factor
 
 
 def imp_count(pt: int) -> int:
     sign = pt // abs(pt) if pt else 0
     imp = 24
-    while abs(pt) < imp_table[imp]:
+    while abs(pt) < IMP_TABLE[imp]:
         imp -= 1
     return sign * imp
 
@@ -105,7 +118,7 @@ def command_handler(call):
                     button('Контра', callback_data='2_factor'),
                     button('Реконтра', callback_data='4_factor'),
                 )
-                bot.send_message(call.message.chat.id, f'Контракт {level}{suits[suit]}:', reply_markup=markup)
+                bot.send_message(call.message.chat.id, f'Контракт {level}{SUITS[suit]}:', reply_markup=markup)
             elif 'factor' in call.data:
                 factor = int(call.data[0])
                 markup = telebot.types.InlineKeyboardMarkup(row_width=3)
@@ -113,7 +126,7 @@ def command_handler(call):
                     button('До зоны', callback_data='0_vul'),
                     button('В зоне', callback_data='1_vul'),
                 )
-                bot.send_message(call.message.chat.id, f'Контракт {level}{suits[suit]} {bonus[factor]}:', reply_markup=markup)
+                bot.send_message(call.message.chat.id, f'Контракт {level}{SUITS[suit]} {BONUS[factor]}:', reply_markup=markup)
             elif 'vul' in call.data:
                 vul = bool(int(call.data[0]))
                 markup = telebot.types.InlineKeyboardMarkup(row_width=3)
@@ -124,9 +137,9 @@ def command_handler(call):
                     button('+4', callback_data='+4_trick'), button('+5', callback_data='+5_trick'), button('+6', callback_data='+6_trick'),
                     button('==', callback_data=' 0_trick'),
                 )
-                bot.send_message(call.message.chat.id, f'Контракт {level}{suits[suit]} {bonus[factor]} {vulnerable[vul]}. Взяток:', reply_markup=markup)
+                bot.send_message(call.message.chat.id, f'Контракт {level}{SUITS[suit]} {BONUS[factor]} {VULNERABLE[vul]}. Взяток:', reply_markup=markup)
             elif 'trick' in call.data:
-                bot.send_message(call.message.chat.id, f'Контракт {level}{suits[suit]} {bonus[factor]} {vulnerable[vul]}:')
+                bot.send_message(call.message.chat.id, f'Контракт {level}{SUITS[suit]} {BONUS[factor]} {VULNERABLE[vul]}:')
                 tricks = int(call.data[:2])
                 tricks = 7 - level if level + tricks > 7 else tricks
                 if tricks < 0:
@@ -147,7 +160,7 @@ def command_handler(call):
 def calculate(message):
     if message.text.isdigit() and int(message.text) <= 40:
         balance = int(message.text)
-        final = counter + compensa[vul][balance]
+        final = counter + COMPENSA[vul][balance]
         imp = imp_count(final)
         if tricks < 0:
             tr = str(tricks)
@@ -155,8 +168,8 @@ def calculate(message):
             tr = '+'+str(tricks)
         else:
             tr = '=='
-        bot.send_message(message.chat.id, f'Контракт {level}{suits[suit]} {bonus[factor]} {vulnerable[vul]} {tr}: {counter}')
-        bot.send_message(message.chat.id, f'Баланс {balance}рс, компенсация: {compensa[vul][balance]}')
+        bot.send_message(message.chat.id, f'Контракт {level}{SUITS[suit]} {BONUS[factor]} {VULNERABLE[vul]} {tr}: {counter}')
+        bot.send_message(message.chat.id, f'Баланс {balance}рс, компенсация: {COMPENSA[vul][balance]}')
         bot.send_message(message.chat.id, f'Сумма: {final}. Импов: {imp}', reply_markup=menu_markup)
     else:
         bot.send_message(message.chat.id, 'Задайте баланс:')
